a15238416e534d2c1f4dd4bbe540769fc9efd243
DavidMichaelCarter <dmc@dctx.com>
2019-08-24 01:52:46 -0500

    labelled the frequency axis on the DFT graph

--- DrawFFT/DrawFFT.java
+++ DrawFFT/DrawFFT.java
@@ -26,11 +26,13 @@ public class DrawFFT extends JFrame{
   public static int intAmplitudes[] = new int[128];
   public static int frequency = 1;
   
+  //Reset wave to Sine with frequency = 1
   public static void reset(){
     frequency = 1;
     computeSineFFT();
   }
   
+  //Compute FFT for a sine wave
   public static void computeSineFFT(){
     for (int u = 0; u < 256; u++){
           y[u] = new Complex(Math.sin((u+1.0)*frequency/(256.0/(2.0*Math.PI))),0);
@@ -41,12 +43,14 @@ public class DrawFFT extends JFrame{
     ifftY = FFT.ifft(fftY);
   }
   
+  //Compute amplitudes of Complex FFT values
   public static void fftAmplitudes(){
     for (int j = 0; j < 128; j++){
       amplitudes[j] = fftY[j].abs();
     }
   }
   
+  //Convert amplitude values from (double) to (int)
   public static void amplitudesToInt(){
     int upperBound = 500;
     int lowerBound = 0;
@@ -88,6 +92,7 @@ public class DrawFFT extends JFrame{
           }
         });
         
+        //Create "Reset" button that resets data to Sine wave with frequency = 1
         JButton resetButton = new JButton("Reset");
         add(resetButton);
         resetButton.setBounds(90,590,125,25);
@@ -98,6 +103,7 @@ public class DrawFFT extends JFrame{
           }
         });
         
+        //Create button to increase Sine frequency by 1
         JButton freqDownButton = new JButton("Frequency (-)");
         add(freqDownButton);
         freqDownButton.setBounds(25,560,125,25);
@@ -111,12 +117,13 @@ public class DrawFFT extends JFrame{
           }
         });
         
+        //Create cutton to decrease Sine frequency by 1
         JButton freqUpButton = new JButton("Frequency (+)");
         add(freqUpButton);
         freqUpButton.setBounds(153,560,125,25);
         freqUpButton.addActionListener(new ActionListener(){
           public void actionPerformed(ActionEvent e){
-            if (frequency < 128){
+            if (frequency < 127){
               frequency++;
               computeSineFFT();
               repaint();
@@ -130,7 +137,6 @@ public class DrawFFT extends JFrame{
         g.setColor(Color.black);
         g.drawString("CLICK & DRAG TO DRAW A WAVE!", 50, 445);
         g.drawString("FAST FOURIER TRANSFORM", 345, 45);
-        g.drawString("Frequency vs. Amplitude", 365, 565);
         g.drawString("INVERSE FAST FOURIER TRANSFORM", 600, 445);
         g.drawString("\"Fast Fourier Transform on a User-Defined Waveform\" by David Michael Carter (August 2019)", 150, 665);
         
@@ -141,7 +147,7 @@ public class DrawFFT extends JFrame{
           g.drawLine(i+25,(int)(100-Math.round((y[i].re()+1)*50.0)+450), i+25, (int)(100-Math.round((y[i].re()+1)*50.0)+450));
           
           //Vertically interpolate when adjacent Y-values are distant
-          if(i < 255){
+          if (i < 255){
             if(Math.abs((int)Math.round((y[i].re()+1)*50.0)-(int)Math.round((y[i+1].re()+1)*50.0)) > 1){
               g.drawLine(i+25,(int)(100-Math.round((y[i].re()+1)*50.0)+450), i+25, (int)(100-Math.round((y[i+1].re()+1)*50.0)+450));
             }
@@ -154,7 +160,7 @@ public class DrawFFT extends JFrame{
             g.drawLine(2*j+301,intAmplitudes[j]+50 , 2*j+301, intAmplitudes[j]+50);
             
             //Vertically interpolate when adjacent FFT frequencies are distant
-            if(j < 127){
+            if (j < 127){
               if(Math.abs(intAmplitudes[j]-intAmplitudes[j+1]) > 1){
                 g.drawLine(2*j+301, intAmplitudes[j]+50, 2*j+302, intAmplitudes[j+1]+50);
               }
@@ -166,8 +172,8 @@ public class DrawFFT extends JFrame{
           g.drawLine(k+577,(int)(100-Math.round((ifftY[k].re()+1)*50.0)+450), k+577, (int)(100-Math.round((ifftY[k].re()+1)*50.0)+450));
           
           //Vertically interpolate when adjacent Y-values are distant
-           if(k < 255){
-            if(Math.abs((int)(100-Math.round((ifftY[k].re()+1)*50.0)+450)-(int)(100-Math.round((ifftY[k+1].re()+1)*50.0)+450)) > 1){
+           if (k < 255){
+            if (Math.abs((int)(100-Math.round((ifftY[k].re()+1)*50.0)+450)-(int)(100-Math.round((ifftY[k+1].re()+1)*50.0)+450)) > 1){
               g.drawLine(k+577,(int)(100-Math.round((ifftY[k].re()+1)*50.0)+450), k+577, (int)(100-Math.round((ifftY[k+1].re()+1)*50.0)+450));
             }
           }
@@ -183,6 +189,18 @@ public class DrawFFT extends JFrame{
         g.drawLine(301,50,301,550);
         g.drawLine(301,550,557,550);
         g.drawLine(557,50,557,550);
+        
+        int leftBorderFFT = 298;
+        //Draw tick marks on DFT Frequency axis, spaced by 16hz
+        for (int i = 0; i <= 256; i += 32){
+          g.drawLine(301 + i,545,301 + i,555);
+        }
+        //Label frequency axis tick marks
+        for (int j = 0; j <= 128; j += 16){
+          g.drawString(String.valueOf(j), leftBorderFFT + 2*j, 570);
+        }
+        g.drawString("Hz", leftBorderFFT + 128, 595);
+        
         //Draw iFFT window elements
         g.drawLine(577,450,833,450);
         g.drawLine(577,450,577,550);
